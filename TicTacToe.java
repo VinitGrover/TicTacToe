e578253132aa0af6af8f99a174335c2d9661027d
VinitGrover <vinitgrover255@gmail.com>
2022-05-25 19:13:47 +0530

    Added uc1 branch

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,5 +3,12 @@ package com.bridgelabz;
 public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe program");
+
+        char [] ch = new char[10];
+
+        for(int i=1;i<10;i++){
+            ch[i]=' ';
+        }
+        
     }
 }
