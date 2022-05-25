bd31578dafb884170abddec1e61445d16d53e2fe
VinitGrover <vinitgrover255@gmail.com>
2022-05-25 19:19:23 +0530

    Modified uc1

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,19 @@
 package com.bridgelabz;
 
 public class TicTacToe {
+
+    void uc1(char [] ch){
+        for(int i=1;i<10;i++){
+            ch[i]=' ';
+        }
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe program");
 
         char [] ch = new char[10];
-
-        for(int i=1;i<10;i++){
-            ch[i]=' ';
-        }
-        
+        TicTacToe t1 = new TicTacToe();
+        t1.uc1(ch);
     }
 }
